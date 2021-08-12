3de018bf819eb6ac55aa39fa34fa4041b574bfdd
Nickycross <izansise@gmail.com>
2021-08-12 18:37:46 +0200

    init

--- Portfolio.js
+++ Portfolio.js
@@ -2,6 +2,3 @@
 var elements = document.getElementsByClassName("row1");
 var elements = document.getElementsByClassName("row2");
 var elements = document.getElementsByClassName("row3");
-
-//Still trying to fix the Git/Github issue, today (07/22/2021) I focused on finish one of my songs for this summer
-//I think I got all this Git problems fixed... I think
