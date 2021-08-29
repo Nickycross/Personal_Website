663b64b018671f5f7308f340a385a89d70a4be48
Nickycross <izansise@gmail.com>
2021-08-29 22:51:24 +0200

    init

--- Portfolio.js
+++ Portfolio.js
@@ -1,4 +1,9 @@
 //ROWS
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
 var elements = document.getElementsByClassName("row1");
 var elements = document.getElementsByClassName("row2");
 var elements = document.getElementsByClassName("row3");
+
